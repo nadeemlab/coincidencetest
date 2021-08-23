769e03127a93f05e3c541d3787b9a83e9f91867d
James Mathews <mail@jmath.read-books.org>
2021-08-23 03:48:49 -0400

    include better precision p

--- tests/test_probability.py
+++ tests/test_probability.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 
 import coincidencetest
 from coincidencetest.algorithm import calculate_probability_of_multicoincidence
@@ -9,15 +10,15 @@ def do_example(
     cases = [(i, set_sizes, ambient_size) for i in range(min(set_sizes) + 1)]
     outputs = {
         intersection_size :
-        calculate_probability_of_multicoincidence(
+        Decimal(calculate_probability_of_multicoincidence(
             intersection_size = intersection_size,
             set_sizes = set_sizes,
             ambient_size = ambient_size,
-        )
+        ))
         for intersection_size, set_sizes, ambient_size in cases
     }
     cdf = [
-        1 - sum([outputs[j] for j in range(0, i)]) for i in range(len(outputs))
+        Decimal(1) - sum([outputs[j] for j in range(0, i)]) for i in range(len(outputs))
     ]
 
     print('For')
@@ -25,18 +26,19 @@ def do_example(
     print('ambient set size = ' + str(ambient_size))
     print('the distribution of the coincidence degree test statistic is:')
     print('')
-    print('\n'.join(str(item) for item in outputs.items()))
+    print('\n'.join(str(i) + ' : ' + str(p) for i, p in outputs.items()))
     print('')
     print('Total probability: ' + str(sum(outputs.values())))
     print('')
     print('CDF:')
-    print('\n'.join([str((i, cdf[i])) for i in range(len(cdf))]))
+    print('\n'.join([str(i) + ' : ' + str(cdf[i]) for i in range(len(cdf))]))
 
 def basic_running_examples():
     do_example(set_sizes = (3, 3, 3), ambient_size = 5)
     do_example(set_sizes = (3, 4, 5), ambient_size = 7)
     do_example(set_sizes = (5, 7, 10), ambient_size = 20)
-    do_example(set_sizes = (5, 5, 5), ambient_size = 40)
+    do_example(set_sizes = (5, 5, 5), ambient_size = 80)
+    do_example(set_sizes = (10, 10, 10), ambient_size = 100)
 
 if __name__=='__main__':
     basic_running_examples()
