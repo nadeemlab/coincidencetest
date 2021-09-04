ca40a1104687642ff9c6b4f14e98c941ef2025e4
James Mathews <mail@jmath.read-books.org>
2021-09-04 16:33:10 -0400

    adding js first steps

---
+++ webapp/coincidencetest.js
@@ -0,0 +1,44 @@
+function setup(){
+  document.getElementById('uploadbutton').addEventListener('change', openDialog);
+
+  var obj_csv = {
+      size:0,
+      dataFile:[]
+  };
+
+  function openDialog() {
+    // const input = document.querySelector('input[type="file"]');
+    // const file = input.files[0];
+    // input = document.getElementById('uploadbutton')
+    input = document.getElementById('uploadbutton')
+    readImage(input)
+  }
+   
+  function readImage(input) {
+      console.log(input)
+      console.log('...')
+   if (input.files && input.files[0]) {
+   let reader = new FileReader();
+          reader.readAsBinaryString(input.files[0]);
+   reader.onload = function (e) {
+   console.log(e);
+   obj_csv.size = e.total;
+   obj_csv.dataFile = e.target.result
+              console.log(obj_csv.dataFile)
+              parseData(obj_csv.dataFile)
+              document.getElementById('area').innerHTML = obj_csv.dataFile
+
+   }
+   }
+  }
+   
+  function parseData(data){
+      let csvData = [];
+      let lbreak = data.split("\n");
+      lbreak.forEach(res => {
+          csvData.push(res.split("\t"));
+      });
+      console.table(csvData);
+  }
+
+}
