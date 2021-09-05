87c986206c30a4fe3f09bf283432e504f8aad1f5
Jimmy Mathews <mathewj2@mskcc.org>
2021-09-05 02:31:05 -0400

    fca signatures showing in html table

--- webapp/coincidencetest.js
+++ webapp/coincidencetest.js
@@ -1,50 +1,123 @@
 function setup(){
-  document.getElementById('uploadbutton').addEventListener('change', openDialog);
+    document.getElementById('uploadbutton').addEventListener('change', openDialog);
+}
 
-  var obj_csv = {
-      size:0,
-      dataFile:[]
-  };
+function show_tooltip(element) {
+    children = element.children
+    last_child = children[children.length - 1]
+    last_child.hidden = false
+}
 
-  function openDialog() {
-    // const input = document.querySelector('input[type="file"]');
-    // const file = input.files[0];
-    // input = document.getElementById('uploadbutton')
+function unshow_tooltip(element) {
+    children = element.children
+    last_child = children[children.length - 1]
+    last_child.hidden = true
+}
+
+var obj_csv = {
+    size : 0,
+    dataFile : [],
+    data : null,
+};
+
+function openDialog() {
     document.getElementById('progressarea').hidden = false
-    input = document.getElementById('uploadbutton')
-    readImage(input)
+    read_csv(document.getElementById('uploadbutton'))
+}
 
-    setTimeout(function(){
-      document.getElementById('progressarea').hidden = true
-    }, 2000);
-    
-  }
-   
-  function readImage(input) {
-      console.log(input)
-      console.log('...')
-   if (input.files && input.files[0]) {
-   let reader = new FileReader();
-          reader.readAsBinaryString(input.files[0]);
-   reader.onload = function (e) {
-   console.log(e);
-   obj_csv.size = e.total;
-   obj_csv.dataFile = e.target.result
-              console.log(obj_csv.dataFile)
-              parseData(obj_csv.dataFile)
-              document.getElementById('area').innerHTML = obj_csv.dataFile
+function read_csv(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.readAsBinaryString(input.files[0]);
+        reader.onload = function (e) {
+            obj_csv.size = e.total;
+            obj_csv.dataFile = e.target.result
+            parsed = parse_table(obj_csv.dataFile)
+            // document.getElementById('area').innerHTML = create_html_table(parsed)
+            data = {
+                "header" : parsed[0],
+                "samples" : parsed.slice(1),
+                "features" : nested_list_transpose(parsed.slice(1)),
+            }
+            c = find_concepts(data, null, null)
+            let closed_sets = c[0]
+            let dual_sets = c[1]
 
-   }
-   }
-  }
-   
-  function parseData(data){
-      let csvData = [];
-      let lbreak = data.split("\n");
-      lbreak.forEach(res => {
-          csvData.push(res.split("\t"));
-      });
-      console.table(csvData);
-  }
+            document.getElementById('progressarea').hidden = true
 
+            table_rows = []
+            for (let i = 0; i < closed_sets.length; i++) {
+                if (closed_sets[i].length == 1) {
+                    continue
+                }
+                table_rows.push([closed_sets[i].sort().join('; '), dual_sets[i].length, '...'])
+            }
+            table_header = ["Signature", "Frequency (out of " + data["samples"].length + ")", "p-value"]
+
+            p0 = "<table><tr>" + "<th>" + table_header.join("</th><th>") + "</th></tr>"
+            p1 = ""
+            for (let k = 0; k < table_rows.length; k++) {
+                p1 += "<tr><td>" + table_rows[k].join("</td><td>") + "</td></tr>\n"
+            }
+            p2 = "</table>"
+            document.getElementById('resultsarea').innerHTML = p0 + p1 + p2
+
+        }
+    }
+}
+
+function nested_list_transpose(nested_list) {
+    transposed = Array(nested_list[0].length).fill(null)
+    for (let j = 0; j < transposed.length; j++) {
+        transposed[j] = Array(nested_list.length).fill(0)
+    }
+
+    for (let i = 0; i < nested_list.length; i++) {
+        for (let j = 0; j < nested_list[0].length; j++) {
+            transposed[j][i] = nested_list[i][j]
+        }
+    }
+    return transposed
+}
+
+function parse_table(text){
+    let csv_data = []
+    let lines = text.trim().split('\n')
+    csv_data.push(lines[0].split('\t'))
+    for (let i = 1; i < lines.length; i++) {
+        let row = []
+        splitted = lines[i].split('\t')
+        for (let j = 0; j < splitted.length; j++) {
+            if ( splitted[j] == '0' || splitted[j] == '1' ) {
+                row.push(parseInt(splitted[j]))
+            }
+            else {
+                row.push(splitted[j])
+            }
+        }
+        csv_data.push(row)
+    }
+    return csv_data
+}
+
+function create_html_table(data) {
+    txt = ""
+    txt += "<table>"
+    txt += "<tr>"
+    for (let j = 0; j < data[0].length; j++) {
+        txt += "<th>"
+        txt += data[0][j]
+        txt += "</th>"
+    }
+    txt += "</tr>"
+    for (let i = 1; i < data.length; i++) {
+        txt += "<tr>"
+        for (let j=0; j< data[i].length; j++) {
+            txt += "<td>"
+            txt += data[i][j]
+            txt += "</td>"
+        }
+        txt += "</tr>"
+    }
+    return ("Number of lines found: " + (data.length-1) + '\n' + txt)
 }
---
+++ webapp/fca.js
@@ -0,0 +1,175 @@
+
+function arrays_equal(a, b) {
+    if (a.length !== b.length) return false;
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function compute_closure(set, data) {
+    let N = set.length
+    let samples = []
+    for (let i = 0; i < data["samples"].length; i++) {
+        accumulator = 0
+        for (let j = 0; j < set.length; j++) {
+            accumulator += data["samples"][i][set[j]]
+        }
+        if (accumulator == N) {
+            samples.push(i)
+        }
+    }
+    samples.sort()
+
+    let M = samples.length
+    let features = []
+    for (let i = 0; i < data["features"].length; i++) {
+        accumulator = 0
+        for (let j = 0; j < samples.length; j++) {
+            accumulator += data["features"][i][samples[j]]
+        }
+        if (accumulator == M) {
+            features.push(i)
+        }
+    }
+    features.sort()
+
+    return {"closed set" : features, "dual set" : samples}
+}
+
+function already_have(set, sets) {
+    for (I in sets) {
+        other = sets[I]
+        if (arrays_equal(set, other)) {
+            return true
+        }
+    }
+    return false
+}
+
+function get_all_pairs(list) {
+    let results = []
+    for (let i = 0; i < list.length - 1; i++) {
+        for (let j = i + 1; j < list.length; j++) {
+            results.push([list[i], list[j]])
+        }
+    }
+    return results
+}
+
+function get_random_integer(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function get_random_sample(list, size) {
+    let sampled = []
+    while (sampled.length < size) {
+        sample = getRandomInteger(0, list.length - 1)
+        if (sampled.includes(sample)) {
+            continue
+        } else {
+            sampled.push(sample)
+        }
+    }
+    return Array.from(sampled, (i) => list[i]).sort();
+}
+
+function do_pairwise_closures(closed_sets, dual_sets, computed_pairs, level_limit) {
+    let range = Array(closed_sets.length).fill().map((x,i)=>i)
+    let all_pairs = get_all_pairs(range)
+    
+    let a = new Set(all_pairs)
+    let b = new Set(computed_pairs)
+    let index_range = new Set([...a].filter(x => !b.has(x)));
+
+    if (index_range.length == 0) {
+        return
+    }
+
+    if ( !(level_limit == null) ) {
+        new_pairs = get_random_sample(index_range, level_limit)
+    }
+    else {
+        new_pairs = [...index_range]
+    }
+
+    for (I in new_pairs) {
+        pair = new_pairs[I]
+
+        index1 = pair[0]
+        index2 = pair[1]
+
+        a = new Set(closed_sets[index1]);
+        b = new Set(closed_sets[index2]);
+        union = new Set([...a, ...b]);
+        union = [...union].sort()
+        c = compute_closure(union, data)
+
+        if ( !(c["dual set"].length == 0) ) {
+            if ( !(already_have(c["closed set"], closed_sets)) ) {
+                closed_sets.push(c["closed set"])
+                dual_sets.push(c["dual set"])
+                console.log('Pushing...')
+                console.log(c["closed set"])
+                console.log(" now have " + closed_sets.length)
+            }
+        }
+        computed_pairs.push([index1, index2])
+    }
+}
+
+function find_concepts(data, level_limit, max_recursion) {
+    closed_sets = []
+    dual_sets = []
+
+    columns = Array(data['features'].length).fill().map((x,i)=>i)
+    for (I in columns) {
+        feature = columns[I]
+        c = compute_closure([feature], data)
+        if ( !(already_have(c["closed set"], closed_sets)) && !(c["dual set"].length == 0) ) {
+            closed_sets.push(c["closed set"])
+            dual_sets.push(c["dual set"])
+        }
+    }
+
+    computed_pairs = []
+    level = 1
+    while (true) {
+        previous_number_sets = closed_sets.length
+        new_pairs_computed = do_pairwise_closures(closed_sets, dual_sets, computed_pairs, level_limit)
+        new_number_sets = closed_sets.length
+        if ( previous_number_sets == new_number_sets ) {
+            break
+        }
+        if ( !(max_recursion == null) ) {
+            if(level == max_recursion) {
+                break
+            }
+        }
+        level += 1
+    }
+
+    function get_named(indices, names) {
+        named = []
+        for (let k = 0; k < indices.length; k++) {
+            named.push(names[indices[k]])
+        }
+        return named
+    }
+
+    for (let i = 0; i < closed_sets.length; i++) {
+        console.log(closed_sets[i])
+        console.log(closed_sets[i].toString())
+    }
+
+    named_signatures = Array.from(closed_sets, (s) => get_named(s, data["header"]))
+
+    for (let i = 0; i < named_signatures.length; i++) {
+        console.log(named_signatures[i])
+        console.log(named_signatures[i].toString())
+    }
+    return [named_signatures, dual_sets]
+}
+
