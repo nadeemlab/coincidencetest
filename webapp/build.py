d8c23b7f7b02a94b82a614f4aed5f034d113bddb
Jimmy Mathews <mathewj2@mskcc.org>
2021-09-06 19:22:34 -0400

    asynchronous reporting on feature sets implemented

--- webapp/build.py
+++ webapp/build.py
@@ -7,12 +7,6 @@ page = open('layout.html').read()
 style_css = open('style.css', 'rt').read()
 page = page.replace('{{style.css}}', style_css)
 
-coincidencetest_js = open('coincidencetest.js', 'rt').read()
-page = page.replace('{{coincidencetest.js}}', coincidencetest_js)
-
-fca_js = open('fca.js', 'rt').read()
-page = page.replace('{{fca.js}}', fca_js)
-
 utilities_js = open('utilities.js', 'rt').read()
 page = page.replace('{{utilities.js}}', utilities_js)
 
@@ -24,10 +18,16 @@ loader = open('loader.svg', 'rb').read()
 loader = str(base64.b64encode(loader), 'utf-8')
 page = page.replace('{{loader-icon-base64}}', loader)
 
-
 example_tsv = open('multiplexed_1500.tsv', 'rb').read()
 example_tsv = str(base64.b64encode(example_tsv), 'utf-8')
 data_uri = 'data: text/tab-separated-values;base64,' + example_tsv
 page = page.replace('{{multiplexed_1500.tsv}}', data_uri)
+
+worker = open('_worker.js').read()
+coincidencetest_js = open('coincidencetest.js', 'rt').read()
+worker = worker.replace('{{coincidencetest.js}}', coincidencetest_js)
+fca_js = open('fca.js', 'rt').read()
+worker = worker.replace('{{fca.js}}', fca_js)
+open('worker.js', 'wt').write(worker)
 
 open('index.html', 'w').write(page)
--- webapp/utilities.js
+++ webapp/utilities.js
@@ -14,53 +14,42 @@ function unshow_tooltip(element) {
     last_child.hidden = true
 }
 
-var tsv_object = {
-    size : 0,
-    data : null,
-};
-
-var signatures_object = {
-    closed_sets : [],
-    dual_sets : [],
-    p_values : [],
-    signatures : null,
+function openDialog() {
+    document.getElementById('progressarea').hidden = false
+    read_tsv(document.getElementById('uploadbutton'))
 }
 
-function get_frequencies(feature_indices) {
-    frequencies = []
-    for (let k = 0; k < feature_indices.length; k++) {
-        i = feature_indices[k]
-        feature = tsv_object.data["features"][i]
-        sum = feature.reduce((a, b) => a + b, 0)
-        frequencies.push(sum)
-    }
-    return frequencies
-}
-
-function do_coincidence_tests() {
-    let number_samples = tsv_object.data["samples"].length
-    let number_features = tsv_object.data["features"].length
-    signatures_object.p_values = []
-    for (let i = 0; i < signatures_object.closed_sets.length; i++) {
-        let incidence_statistic = dual_sets[i].length
-        let frequencies = get_frequencies(closed_sets[i])
-        console.log(incidence_statistic + ", " + frequencies + ", " + number_samples)
-        let p = coincidencetest(incidence_statistic, frequencies, number_samples, number_features)
-        console.log("pvalue: " + p)
-        signatures_object.p_values.push(p)
+function read_tsv(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.readAsBinaryString(input.files[0]);
+        reader.onload = function (event) {
+            spawn_worker(event)
+        }
     }
 }
 
-function do_one_coincidence_test() {
-    let number_samples = tsv_object.data["samples"].length
-    let number_features = tsv_object.data["features"].length
-    i = signatures_object.closed_sets.length - 1
-    let incidence_statistic = dual_sets[i].length
-    let frequencies = get_frequencies(closed_sets[i])
-    console.log(incidence_statistic + ", " + frequencies + ", " + number_samples)
-    let p = coincidencetest(incidence_statistic, frequencies, number_samples, number_features)
-    console.log("pvalue: " + p)
-    signatures_object.p_values.push(p)
+function spawn_worker(event) {
+    if (typeof(worker) == "undefined") {
+        worker = new Worker("./worker.js");
+        // worker = new Worker(URL.createObjectURL(new Blob([worker_function.toString()], {type: 'text/javascript'})));
+        // worker = new Worker(URL.createObjectURL(new Blob([worker_function.toString()], {type: 'text/javascript'})));
+        console.log('Spawned worker.')
+        worker.onmessage = onmessage
+        worker.postMessage(event.target.result)
+    }
+}
+
+function onmessage(event) {
+    console.log('Received message from worker.')
+
+    if (event.data == "Done with concepts.") {
+        document.getElementById('progressarea').hidden = true
+    }
+    else {
+        signatures_object = event.data
+        show_signatures(signatures_object)
+    }
 }
 
 function classify_p_value(p) {
@@ -73,7 +62,7 @@ function classify_p_value(p) {
     return ' class="insignificant"'
 }
 
-function show_signatures() {
+function show_signatures(signatures_object) {
     o = signatures_object
     table_rows = []
     for (let i = 0; i < o.closed_sets.length; i++) {
@@ -84,7 +73,7 @@ function show_signatures() {
     }
     table_rows.sort(function(row1, row2) {return row1[2] - row2[2]})
 
-    table_header = ["Signature", "Frequency<br>(out of " + tsv_object.data["samples"].length + ")", "p-value"]
+    table_header = ["Signature", "Frequency<br>(out of " + signatures_object.total_samples + ")", "p-value"]
 
     p0 = '<table class="signaturestable"><tr>' + "<th>" + table_header.join("</th><th>") + "</th></tr>"
     p1 = ""
@@ -94,72 +83,4 @@ function show_signatures() {
     }
     p2 = "</table>"
     document.getElementById('resultsarea').innerHTML = p0 + p1 + p2
-}
-
-function openDialog() {
-    document.getElementById('progressarea').hidden = false
-    read_tsv(document.getElementById('uploadbutton'))
-}
-
-function pipeline(event) {
-    tsv_object.size = event.total;
-    parsed = parse_table(event.target.result)
-    tsv_object.data = {
-        "header" : parsed[0],
-        "samples" : parsed.slice(1),
-        "features" : nested_list_transpose(parsed.slice(1)),
-    }
-    single_addition_callback = function(c) {
-        signatures_object.closed_sets.push(get_named(c["closed set"], tsv_object.data["header"]))
-        signatures_object.dual_sets.push(c["dual set"])
-        do_one_coincidence_test()
-        show_signatures()
-    }
-
-    c = find_concepts(tsv_object.data, null, null, single_addition_callback)
-    document.getElementById('progressarea').hidden = true
-}
-
-function read_tsv(input) {
-    if (input.files && input.files[0]) {
-        let reader = new FileReader();
-        reader.readAsBinaryString(input.files[0]);
-        reader.onload = function (event) {
-            pipeline(event)
-        }
-    }
-}
-
-function nested_list_transpose(nested_list) {
-    transposed = Array(nested_list[0].length).fill(null)
-    for (let j = 0; j < transposed.length; j++) {
-        transposed[j] = Array(nested_list.length).fill(0)
-    }
-
-    for (let i = 0; i < nested_list.length; i++) {
-        for (let j = 0; j < nested_list[0].length; j++) {
-            transposed[j][i] = nested_list[i][j]
-        }
-    }
-    return transposed
-}
-
-function parse_table(text){
-    let rows = []
-    let lines = text.trim().split('\n')
-    rows.push(lines[0].split('\t'))
-    for (let i = 1; i < lines.length; i++) {
-        let row = []
-        splitted = lines[i].split('\t')
-        for (let j = 0; j < splitted.length; j++) {
-            if ( splitted[j] == '0' || splitted[j] == '1' ) {
-                row.push(parseInt(splitted[j]))
-            }
-            else {
-                console.warn("Table data should be binary, '1' or '0'.")
-            }
-        }
-        rows.push(row)
-    }
-    return rows
 }
