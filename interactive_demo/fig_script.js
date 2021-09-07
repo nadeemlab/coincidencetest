50740a95336fd5d21f020e82a21c4dd894b45d70
Jimmy Mathews <mathewj2@mskcc.org>
2021-09-07 15:39:01 -0400

    including interactive demo

---
+++ interactive_demo/fig_script.js
@@ -0,0 +1,146 @@
+
+data_obj = {
+    data : [
+        [1,1,1,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,1,1],
+        [0,0,1,0,1,1,0,0,1,1,0,1,1,0,0,1,0,1,1,1],
+        [0,0,1,1,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1],
+        [0,1,0,0,0,0,0,1,0,0,1,1,0,0,1,1,0,1,1,1],
+    ]
+}
+
+function retrieve_data() {
+    for (let j = 0; j < data_obj.data.length; j++) {
+        for (let i =0; i < data_obj.data[0].length; i++) {
+            let element = document.getElementById(i + ';' + j)
+            if (element.classList.contains('one')) {
+                data_obj.data[j][i] = 1
+            } else {
+                data_obj.data[j][i] = 0
+            }
+        }
+    }
+    return data_obj.data
+}
+
+function sum(row) {
+    let accumulator = 0
+    for (let k = 0; k < row.length ; k++) {
+        accumulator += row[k]
+    }
+    return accumulator
+}
+
+function get_frequency(j) {
+    return sum(data_obj.data[j])
+}
+
+function get_frequencies(data) {
+    let frequencies = []
+    for (let k = 0; k < data.length ; k++) {
+        frequencies.push(sum(data[k]))
+    }
+    return frequencies
+}
+
+function get_intersections(data) {
+    let included = 0
+    for (let i = 0; i < data[0].length; i++) {
+        let number_in = 0
+        for (let j = 0; j < data.length; j++) {
+            if (data[j][i] == 1) {
+                number_in += 1
+            }
+        }
+        if (number_in == data_obj.data.length) {
+            included += 1
+        }
+    }
+    return included
+}
+
+function fill_fields(data) {
+    let frequencies = get_frequencies(data)
+    let number_intersections = get_intersections(data)
+
+    document.getElementById("numfeatures").innerText = data.length + ""
+    document.getElementById("frequencies").innerText = frequencies.join(', ')
+
+    let inter = document.getElementsByClassName("numintersections")
+    for (let k = 0; k < inter.length; k++) {
+        inter[k].innerText = "" + number_intersections
+    }
+
+    for (let j = 0; j < data.length; j++) {
+        document.getElementById("F" + j).innerText = get_frequency(j)
+    }
+
+    let ambient_size = data[0].length
+    let intersection_size = number_intersections
+    let set_sizes = frequencies
+    let singlep = calculate_probability_of_multicoincidence(ambient_size, set_sizes, intersection_size)
+    let pvalue = coincidencetest(intersection_size, set_sizes, ambient_size, null)
+
+    document.getElementById("singlep").innerText = singlep
+    document.getElementById("pvalue").innerText = pvalue
+
+    draw_p_meters()
+}
+
+function handle_click(element) {
+    element.classList.toggle('zero');
+    element.classList.toggle('one');
+    fill_fields(retrieve_data())
+}
+
+function change_highlight_intersections(flag) {
+    let data = data_obj.data
+    for (let i = 0; i < data[0].length; i++) {
+        let number_in = 0
+        for (let j = 0; j < data.length; j++) {
+            if (data[j][i] == 1) {
+                number_in += 1
+            }
+        }
+        if (number_in == data_obj.data.length) {
+            for (let j = 0; j < data.length; j++) {
+                let element = document.getElementById(i + ';' + j)
+                if (flag) {
+                    element.classList.add('highlight')
+                } else {                    
+                    element.classList.remove('highlight')
+                }
+            }
+        }
+    }
+}
+
+function highlight_intersections() {
+    change_highlight_intersections(true)
+}
+
+function unhighlight_intersections() {
+    change_highlight_intersections(false)    
+}
+
+function draw_p_meter(element_name, value) {
+    var c = document.getElementById(element_name);
+    var ctx = c.getContext("2d");
+    ctx.beginPath()
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.lineWidth = "1"
+    ctx.strokeStyle = "#999999"
+    ctx.fillStyle = "#a6ff97"
+    // ctx.fillRect(1, 1, c.width-1, c.height-1);
+    ctx.fillRect(0,0, parseInt(c.width * value), c.height)
+    ctx.rect(-1,0, parseInt(c.width * value), c.height)
+    ctx.stroke();
+}
+
+function draw_p_meters() {
+    singlep = parseFloat(document.getElementById("singlep").innerText)
+    pvalue = parseFloat(document.getElementById("pvalue").innerText)
+    draw_p_meter("pmeter_singlep", singlep)
+    draw_p_meter("pmeter_p", pvalue)
+}
+
+// fill_fields(data_obj.data)
---
+++ interactive_demo/generate_example_heatmap.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import sys
+
+import coincidencetest
+from coincidencetest import coincidencetest
+from coincidencetest._coincidencetest import calculate_probability_of_multicoincidence
+
+data = [
+    [1,1,1,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,1,1],
+    [0,0,1,0,1,1,0,0,1,1,0,1,1,0,0,1,0,1,1,1],
+    [0,0,1,1,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1],
+    [0,1,0,0,0,0,0,1,0,0,1,1,0,0,1,1,0,1,1,1],
+]
+
+def create_html(data):
+    intersections = [
+        i for i in range(len(data[0])) if all(row[i]==1 for row in data)
+    ]
+
+    style = '''
+    body {
+        font-family: sans-serif;
+        font-size: 1.2em;
+    }
+    div.figdiv {
+        text-align: center;
+    }
+    div.tableheatbounder {
+        background-color: #eeeeee;
+        display: inline-block;
+        padding: 15px;
+    }
+    p.tabletitle {
+        text-align: center;
+    }
+    table.heat {
+        border-spacing: 3px;
+        padding: 10px;
+        display: inline;
+    }
+    table.heat tr th {
+        padding: 5px;
+    }
+    table.heat tr td {
+        padding: 25px 10px 10px 25px;
+        text-align: center;
+    }
+
+    table.heat tr td.zero {
+        /* background: #F0FFFF; */
+        background: #e9e3f8;
+    }
+    table.heat tr td.zero:hover {
+        background: #cdbbf4;
+    }
+
+    table.heat tr td.one {
+        /* background: #F08080; */
+        background: #f25b24;
+    }
+    table.heat tr td.one:hover {
+        background: #f6a476;
+    }
+
+    table.heat tr td.highlight {
+        background: lightgray;
+    }
+
+    div.tablestatsbounder {
+        background-color: #eeeeee;
+        display: inline-block;
+        padding: 15px;
+    }
+
+    table.stats {
+        display: inline;
+    }
+
+    table.stats tr td.pvals {
+        width: 20ch;
+    }
+
+    table.stats tr td {
+        text-align: left;
+        padding: 15px;
+    }
+
+    .numintersections {
+        font-weight: bold;
+    }
+
+    .numintersections:hover {
+        text-decoration: underline dotted;
+        cursor: default;
+    }
+
+    #captiontext {
+        padding: 0 30% 0 30%;
+        text-align: left;
+    }
+
+    #pmeter_p {
+        border: 1px solid gray;
+        width: 180px;
+        height: 15px;
+    }
+
+    #pmeter_singlep {
+        border: 1px solid gray;
+        width: 180px;
+        height: 15px;
+    }
+
+    '''
+    def wrap(pre, inner, post):
+        return pre + inner + post
+
+    def find_class(j, i):
+        classes = []
+
+        if data[j][i] == 0:
+            classes.append('zero')
+
+        if data[j][i] == 1:
+            classes.append('one')
+
+        return ' '.join(classes)
+
+    frequencies = [sum(row) for j, row in enumerate(data)]
+
+    data_rows = [
+        ''.join(
+            '<td id="' + str(i) +  ';' + str(j) + '" onclick="handle_click(this)" class="' + find_class(j, i) + '"></td>'
+            for i, entry in enumerate(row)
+        ) + '<td><span id="F' + str(j) + '">' + str(frequencies[j]) + '</span></td>'
+        for j, row in enumerate(data)
+    ]
+
+    table = wrap(
+        '<table class="heat">',
+        '<th></th>'*(len(data[0])) + '<th>frequency</th>' + wrap(
+            '<tr>',
+            '</tr><tr>'.join(data_rows),
+            '</tr>',
+        ),
+        '</table>',
+    )
+    table = wrap('<div class="tableheatbounder">', table, '</div>')
+    title = '<p class="tabletitle"><span id="numfeatures">%s</span> features coinciding along <span class="numintersections" onmouseenter="highlight_intersections()"" onmouseleave="unhighlight_intersections()">%s</span> samples</p>' % (len(data), len(intersections))
+
+    single_p = calculate_probability_of_multicoincidence(len(data[0]), frequencies, len(intersections))
+    p = coincidencetest(len(intersections), frequencies, len(data[0]))
+
+    single_p = str(single_p)[0:9]
+    p = str(p)[0:9]
+
+    caption = '<br><p id="captiontext">Among all configurations of 4 binary features with frequencies (<span id="frequencies">' + ', '.join(str(f) for f in frequencies)+ '</span>), the probabilities of the configuration shown above (or more extreme) are given below:<br><div class="tablestatsbounder"><table class="stats"><tr><td>Probability of exactly <span onmouseenter="highlight_intersections()"" onmouseleave="unhighlight_intersections()" class="numintersections">' + str(len(intersections)) + '</span> coincidents</td><td class="pmeter"><canvas id="pmeter_singlep"></span></td><td class="pvals"><span id="singlep">' + str(single_p) + '</span></td></tr><td>Probability of <span class="numintersections" onmouseenter="highlight_intersections()"" onmouseleave="unhighlight_intersections()">' + str(len(intersections)) + '</span> or more coincidents</td><td class="pmeter"><canvas id="pmeter_p"></span></td><td class="pvals"><span id="pvalue">' + str(p) + '</span></td><tr></tr></table></div>'
+
+    if 'no-caption' in sys.argv:
+        caption = ''
+
+    html = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><style>' + style + '</style><script>{{coincidencetest.js}}</script><script>{{fig_script.js}}</script></head><body onload="fill_fields(retrieve_data())">' + '<div class="figdiv">' + table + title + caption + '</div></body></html>'
+    return html
+
+html = create_html(data)
+
+coincidencetest_js = open('../webapp/coincidencetest.js', 'rt').read()
+html = html.replace('{{coincidencetest.js}}', coincidencetest_js)
+
+fig_script_js = open('fig_script.js', 'rt').read()
+html = html.replace('{{fig_script.js}}', fig_script_js)
+
+open('fig.html', 'wt').write(html)
+
